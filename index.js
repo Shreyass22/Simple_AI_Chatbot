7aa28f3eea416ae4a158c991abd44fc5886a5d95
Shreyas Jaiswal <47319276+Shreyass22@users.noreply.github.com>
2020-07-31 18:52:56 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 require('dotenv').config()
 const APIAI_TOKEN = process.env.APIAI_TOKEN;
 const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+const path = require('path')
 
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/views')); // html
-app.use(express.static(__dirname + '/public'));// js, css, images
+app.use(express.static(path.join(__dirname , './views'))); // html
+app.use(express.static(path.join(__dirname , './public')));// js, css, images
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
